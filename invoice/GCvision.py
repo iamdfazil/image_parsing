27144df9b8ce4c747bf28f454ea4e66c4f627c4a
Mohamed Fazil <108261732+iamdfazil@users.noreply.github.com>
2024-07-18 16:19:33 +0530

    Update GCvision.py

--- invoice/GCvision.py
+++ invoice/GCvision.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 # Set Google Cloud credentials environment variable
-service_account_path = "/etc/secrets/zeta-ascent-425607-d8-93027003e046"
+service_account_path = "/etc/secrets/zeta-ascent-425607-d8-93027003e046.json"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_account_path
 
 # Ensure the uploads directory exists
